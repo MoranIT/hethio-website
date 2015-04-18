045591162786fb4a995ae885945d5e03bb843da2
Daniel Moran <danielheth@hotmail.com>
2015-04-17 20:45:06 -0500

    adjusting rewritebase for api

--- app/api/index.php
+++ app/api/index.php
@@ -5,14 +5,20 @@
  */
 require_once ("common.php");
 
+$app = new \Slim\Slim();
 
 
+$app->get('/raw', 'getRaw');
+$app->get('/raw/:id', 'getRaw');
 function getRaw($id = null) {
     $sql_query = "select * FROM raw";
     if (!is_null($id)) $sql_query .= " WHERE id='".$id."'";
     echo fetchRows($sql_query);
 }
 
+
+$app->get('/agent', 'getAgent');
+$app->get('/agent/:agent', 'getAgent');
 function getAgent($agent = null) {
     $sql_query = "select * FROM agent";
     if (!is_null($agent)) $sql_query .= " WHERE agent='".$agent."'";
@@ -21,20 +27,17 @@ function getAgent($agent = null) {
 
 
 
+$app->get('/speedtest', 'getSpeedTest');
+$app->get('/speedtest/:id', 'getSpeedTest');
+function getSpeedTest($id = null) {
+    $sql_query = "select * FROM agent";
+    if (!is_null($agent)) $sql_query .= " WHERE speedtest='".$id."'";
+    echo fetchRows($sql_query);
+}
 
 
 
-$app = new \Slim\Slim();
 
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
-
-$app->get('/agent', 'getAgent');
-$app->get('/agent/:agent', 'getAgent');
-
-$app->get('/raw', 'getRaw');
-$app->get('/raw/:id', 'getRaw');
 
 
 $app->run();
